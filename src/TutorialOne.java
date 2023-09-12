750cd9f4b9302ec5cceb16664ee777561f814d74
chronosonder <catalinbuhan@gmail.com>
2023-09-12 11:55:07 +0100

    1B complete

--- src/TutorialOne.java
+++ src/TutorialOne.java
@@ -9,7 +9,7 @@ public class TutorialOne {
 
         card1  = randomNum();
         card2 = randomNum();
-        total  = card1 + card2;
+        total  = calcTotal(card1, card2);
 
         System.out.println("Your Hand");
         System.out.printf("First Card --> %d%nSecond Card --> %d%n", card1 ,card2);
@@ -24,21 +24,38 @@ public class TutorialOne {
             System.out.printf("%nNew Card --> %d%n", newCard);
             System.out.printf("Total %d%n%n", total);
 
-            System.out.println("Do you wish to draw another card? (y/n)");
-            System.out.print("Choice: ");
-            choice = kInput.next();
-
+            if (busted(total)){
+                System.out.println("Busted!");
+                choice = "n";
+            } else {
+                System.out.println("Do you wish to draw another card? (y/n)");
+                System.out.print("Choice: ");
+                choice = kInput.next();
+            }
         }
 
-        if ((total>19) && (total<=21)){
+        if (getResult(total)){
             System.out.println("You Won");
         } else {
             System.out.println("You Lost");
         }
 
     }
+
+
     public static int randomNum() {
         return (int)(Math.random()*10)+1;
+    }
+
+    public static int calcTotal(int cardOne, int cardTwo) {
+        return cardOne + cardTwo;
+    }
+
+    public static boolean busted(int cardsTotal) {
+        return cardsTotal>21;
+    }
+    public static boolean getResult(int totalCards){
+        return ((totalCards > 18) && (totalCards <= 21));
     }
 
 }
