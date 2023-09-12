6b465cad075dd67f34ed82586b2d8229fb7702a3
chronosonder <catalinbuhan@gmail.com>
2023-09-12 11:25:45 +0100

    1A complete

--- src/TutorialOne.java
+++ src/TutorialOne.java
@@ -13,8 +13,7 @@ public class TutorialOne {
 
         System.out.println("Your Hand");
         System.out.printf("First Card --> %d%nSecond Card --> %d%n", card1 ,card2);
-        System.out.println();
-        System.out.printf("Total %d%n", total);
+        System.out.printf("Total %d%n%n", total);
         System.out.println("Do you wish to draw another card? (y/n)");
         System.out.print("Choice: ");
         choice = kInput.next();
@@ -22,13 +21,19 @@ public class TutorialOne {
         while (choice.equals("y")){
             newCard = randomNum();
             total += newCard;
-            System.out.printf("%nNew Card --> %d", newCard);
+            System.out.printf("%nNew Card --> %d%n", newCard);
+            System.out.printf("Total %d%n%n", total);
 
-            System.out.println();
             System.out.println("Do you wish to draw another card? (y/n)");
             System.out.print("Choice: ");
             choice = kInput.next();
 
+        }
+
+        if ((total>19) && (total<=21)){
+            System.out.println("You Won");
+        } else {
+            System.out.println("You Lost");
         }
 
     }
