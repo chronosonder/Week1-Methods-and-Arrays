425dcd4ba3723d3a7e37373c71ac1d8e935457c5
chronosonder <catalinbuhan@gmail.com>
2023-09-15 12:05:12 +0100

    Started tutorial two from Week1 - added menu method

---
+++ src/TutorialTwo.java
@@ -0,0 +1,31 @@
+/*
+Author: Cat
+Version 0.1
+
+Starting tutorial 2
+
+Date 15/09/23
+ */
+
+
+public class TutorialTwo {
+
+    public static void main(String[] args){
+
+        displayMenu();
+    }
+
+    public static void displayMenu() {
+        System.out.println("*Welcome to QuackRlines*");
+        System.out.println("Please choose from one of the options below by entering the corresponding number");
+        System.out.println("1. Book a seat");
+        System.out.println("2. Display all seats showing passenger details");
+        System.out.println("3. Cancel a seat");
+        System.out.println("4. Display report of total takings");
+        System.out.println();
+        System.out.println("5. Exit");
+    }
+
+
+}
+
