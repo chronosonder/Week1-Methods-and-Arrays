ac6416dce71f8a2061bc5f6ba51ac50ac7881f20
chronosonder <catalinbuhan@gmail.com>
2023-09-15 13:43:03 +0100

    Working on option 1

--- src/TutorialTwo.java
+++ src/TutorialTwo.java
@@ -1,18 +1,84 @@
 /*
 Author: Cat
-Version 0.1
+Version 0.3
 
-Starting tutorial 2
+Java Tutorial 2 Week 1
 
 Date 15/09/23
  */
 
 
-public class TutorialTwo {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class TutorialTwo {
+    static Scanner kInput = new Scanner(System.in);
     public static void main(String[] args){
+        String[][] planeSeats = new String[14][3];
+        int choice, seatNumber;
+
+        planeSeats[0][0] = "Cat";
+        planeSeats[0][1] = "Buhan";
+        planeSeats[0][2] = "1";
+        planeSeats[1][0] = "Mike";
 
         displayMenu();
+        System.out.printf("%nChoice: ");
+        choice = kInput.nextInt();
+
+        switch (choice) {
+            case 1:
+                availableSeats(planeSeats);
+
+                System.out.printf("%nEnter the seat number you would like to book: ");
+                seatNumber = kInput.nextInt() - 1;
+
+                System.out.println("Please enter your first name.");
+                planeSeats[seatNumber][0] = kInput.next();
+
+                System.out.println("Please enter your last name.");
+                planeSeats[seatNumber][1] = kInput.next();
+
+                System.out.println("Please select the booking type.");
+                System.out.printf("1 - Business     2 - Local       3 - Leisure");
+                planeSeats[seatNumber][2] = kInput.next();
+
+                System.out.println("Thank you for your booking.");
+                availableSeats(planeSeats);
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("Incorrect choice.");
+                break;
+        }
+
+
+
+    }
+
+    public static void availableSeats(String[][] seatsArray) {
+
+        System.out.print("Available seat numbers: ");
+
+        for (int row = 0; row < 14; row++) {
+            if (seatsArray[row][0] == null){
+
+                System.out.printf("%d%s", row + 1,(row == 13)?".":", ");
+            }
+//            for (int col = 0; col < 3; col++){
+//                System.out.println(row+" "+col);
+//            }
+
+        }
+
     }
 
     public static void displayMenu() {
@@ -26,6 +92,9 @@ public class TutorialTwo {
         System.out.println("5. Exit");
     }
 
+    public static void seatBooking() {
+
+    }
 
 }
 
